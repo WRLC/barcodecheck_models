00f31ec862f11c6df63697cdc2ed47b22bf1b691
Tom Boone <tbc@tomboone.com>
2025-03-18 11:40:17 -0400

    add database connection and session

--- barcodecheck_models/extensions.py
+++ barcodecheck_models/extensions.py
@@ -1,4 +1,12 @@
-from sqlalchemy.orm import DeclarativeBase
+import os
+import dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+dotenv.load_dotenv()
+
+engine = create_engine(os.getenv("SQLALCHEMY_DB_URL"))  # type:ignore[arg-type] # Create a new SQLite database
+session_factory = sessionmaker(bind=engine)  # Create a session factory
 
 
 class Base(DeclarativeBase):
--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ setup(
     name='barcodecheck_models',
     version='0.1',
     description='A package for barcode check models',
+    url='https://github.com/WRLC/barcodecheck_models',
     author='Tom Boone',
     author_email='boone@wrlc.org',
     license='MIT',
     packages=['barcodecheck_models'],
     install_requires=[
         'sqlalchemy',
+        'python-dotenv',
     ],
     zip_safe=False
 )
